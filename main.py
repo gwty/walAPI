d3c3087d976392e5e737c7463992f9c5e78e3e3d
Gowtham Ashok <gwty93@gmail.com>
2018-04-26 16:01:45 -0400

    Added search API and product lookup API support to server

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import io
 from bottle import route, run, static_file
 
 apiKey = 'ufureemfx7a3mzx4mr93xc82'
+
 @route('/recommendations/<item_id>')
 def recommendations(item_id):
     
     req = urllib.request.Request('http://api.walmartlabs.com/v1/nbp?itemId=' + item_id
     + '&format=xml&apiKey=' + apiKey)
     with urllib.request.urlopen(req) as response:
-        the_page = response.read().decode('utf-8')
+        response_xml = response.read().decode('utf-8')
         
-        xml = io.StringIO(the_page)
+        xml = io.StringIO(response_xml)
         xml_data = []
         tree = etree.parse(xml)  
         root = tree.getroot()
@@ -25,6 +26,26 @@ def recommendations(item_id):
 
     json_data = json.dumps(xml_data)
     return json_data
+
+@route('/search/<search_string>')
+def search(search_string):
+    
+    req = urllib.request.Request("http://api.walmartlabs.com/v1/search?query=+" + search_string + "&format=json&apiKey=" +apiKey)
+    with urllib.request.urlopen(req) as response:
+        response_json = response.read()
+        JSON_object = json.loads(response_json.decode('utf-8'))
+
+    return json.dumps(JSON_object)
+
+@route('/lookup/<item_id>')
+def lookup(item_id):
+    
+    req = urllib.request.Request("http://api.walmartlabs.com/v1/items/" + item_id + "?format=json&apiKey=" +apiKey)
+    with urllib.request.urlopen(req) as response:
+        response_json = response.read().decode("utf-8");
+        JSON_object = json.loads(response_json)
+        
+    return json.dumps(JSON_object)
 
 @route('<filename:path>')
 def serve_static_files(filename):
